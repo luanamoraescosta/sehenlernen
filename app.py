45357752956718d3ed3b4126217f8be0152a77fe
luanamcosta <moraescostaluana@gmail.com>
2025-05-06 11:44:15 +0200

    changes

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.decomposition import PCA
-import hdbscan
 import matplotlib.pyplot as plt
 import io
 from PIL import Image
